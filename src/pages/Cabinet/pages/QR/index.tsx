81e8216dc5314f18d6933b4e8048ea1d85f3e287
kirillbushmanov <dissin101@gmail.com>
2022-09-25 12:44:55 +0600

    Add QR-code print

--- src/pages/Cabinet/pages/QR/index.tsx
+++ src/pages/Cabinet/pages/QR/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import {QRCodeSVG} from 'qrcode.react';
-import {Box} from "@mui/material";
+import {Box, Button} from "@mui/material";
 import {useAppSelector} from "../../../../store/hooks";
 
 const QRPage = () => {
     const {token} = useAppSelector((state) => state.auth)
     return (
-        <Box display={'flex'} justifyContent={'center'}>
+        <Box display={'flex'} flexDirection={'column'} justifyContent={'center'} alignItems={'center'}>
             <QRCodeSVG size={256} value={window.location.origin + "/payment?data=" + token.split(' ')[1]} />
+            <Button sx={{marginTop: 2, width: '120px'}} onClick={() => window.print()} variant={'contained'}>Печать</Button>
         </Box>
     );
 };
