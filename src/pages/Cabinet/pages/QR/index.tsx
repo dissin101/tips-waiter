210b7b2ed556ce9ffc0206db3726014af8f42fe7
kirillbushmanov <dissin101@gmail.com>
2022-09-25 14:30:08 +0600

    Add QR Link

--- src/pages/Cabinet/pages/QR/index.tsx
+++ src/pages/Cabinet/pages/QR/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {QRCodeSVG} from 'qrcode.react';
-import {Box, Button} from "@mui/material";
+import {Box, Button, TextField} from "@mui/material";
 import {useAppSelector} from "../../../../store/hooks";
 
 const QRPage = () => {
@@ -9,6 +9,10 @@ const QRPage = () => {
         <Box display={'flex'} flexDirection={'column'} justifyContent={'center'} alignItems={'center'}>
             <QRCodeSVG size={256} value={window.location.origin + "/payment?data=" + token.split(' ')[1]} />
             <Button sx={{marginTop: 2, width: '120px'}} onClick={() => window.print()} variant={'contained'}>Печать</Button>
+            <TextField
+                sx={{marginTop: 2, minWidth: '320px'}}
+                label={'Копировать ссылку'}
+                value={window.location.origin + "/payment?data=" + token.split(' ')[1]}/>
         </Box>
     );
 };
